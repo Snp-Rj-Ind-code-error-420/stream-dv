19f25b4f459c60b093f51d0cb4efd9cc798265a4
Raja Sardar <80396878+Snp-Rj-Ind-code-error-420@users.noreply.github.com>
2025-03-20 12:36:17 +0530

    add some basic of streamlit

--- main.py
+++ main.py
@@ -3,3 +3,13 @@ import pandas as pd
 import numpy as np 
 import matplotlib.pyplot as plt 
 import seaborn as sns
+
+# st.title("dv")
+st.set_page_config(page_title="dv",
+	page_icon="🤣")
+st.title("data visualization with streamit")
+with st.sidebar:
+	uplode=st.file_uploader("uplodde csv")
+if uplode is not None:
+	df=pd.read_csv(uplode)
+	st.dataframe(df.head())
